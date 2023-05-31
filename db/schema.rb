eeb30494c5c18dfe98d49bee599f15b526b05f60
ricardomendes <ricardogaditmendes26@gmail.com>
2023-05-31 16:18:27 +0100

    Adding email to users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_31_150347) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_31_151449) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
