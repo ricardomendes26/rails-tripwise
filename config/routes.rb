19054a45cf74b0832e0f5612e6e8cdf297fad69d
ricardomendes <ricardogaditmendes26@gmail.com>
2023-05-31 18:18:20 +0100

    profile page, adding a new user, feed page

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "users", to: "users#index"
+  get "users/new", to: "users#new"
+  post "users", to: "users#create"
+  get "users/:id", to: "users#show", as: :user
 end
